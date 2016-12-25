fc31b3a7f66b5c529bdee7b145eba7ba21c585e2
Paul Friedman <paulrfri@gmail.com>
2016-12-25 15:21:22 -0500

    Replace brunch with webpack

---
+++ web/static/js/Melo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Melo extends React.Component {
+  render () {
+    return (
+      <div>it's melo?!?</div>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const { resolve } = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: [
+    './js/app.js',
+    './css/app.css'
+  ],
+  output: {
+    filename: 'js/app.js',
+    path: resolve(__dirname, 'priv', 'static')
+  },
+  context: resolve(__dirname, 'web', 'static'),
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: 'css-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new ExtractTextPlugin('css/app.css'),
+    new CopyWebpackPlugin([{ from: "./assets" }])
+  ]
+}
